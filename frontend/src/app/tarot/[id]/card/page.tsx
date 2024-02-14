bd8877bd1a2ccd905911a2a1e3163192f2a94870
br j <honuj9983@gmail.com>
2024-02-14 17:39:40 +0900

    [frontend]  disabled error cod

--- frontend/src/app/tarot/[id]/card/page.tsx
+++ frontend/src/app/tarot/[id]/card/page.tsx
@@ -3,23 +3,23 @@ import { tarotCardList, CardImageList } from "@/app/lib/card";
 import Image from "next/image";
 import Card from "@/app/components/card";
 
-export const getCard = async (cardId: string) => {
-  // const res = await fetch(`https://api.scryfall.com/cards/${cardId}`);
-  // const card = await res.json();
-  console.log(typeof cardId);
-  console.log(`cardId: ${cardId}`);
-  console.log(typeof tarotCardList);
-  const cardData = tarotCardList[cardId];
-  console.log("잘나오냐", cardData);
-  const card = {
-    id: cardId,
-    name: cardData[1],
-    // image: `https://upload.wikimedia.org/wikipedia/commons/${cardData[2]}.jpg`,
-    image: `/tarot/${cardData[2]}.jpg`,
-  };
+// export const getCard = async (cardId: string) => {
+//   // const res = await fetch(`https://api.scryfall.com/cards/${cardId}`);
+//   // const card = await res.json();
+//   console.log(typeof cardId);
+//   console.log(`cardId: ${cardId}`);
+//   console.log(typeof tarotCardList);
+//   const cardData = tarotCardList[cardId];
+//   console.log("잘나오냐", cardData);
+//   const card = {
+//     id: cardId,
+//     name: cardData[1],
+//     // image: `https://upload.wikimedia.org/wikipedia/commons/${cardData[2]}.jpg`,
+//     image: `/tarot/${cardData[2]}.jpg`,
+//   };
 
-  return card;
-};
+//   return card;
+// };
 
 const Page = ({ params }: any) => {
   const id = params.id;
